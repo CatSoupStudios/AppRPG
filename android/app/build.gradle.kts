c02bd46d1f8c1f3380bc6a5aa9d73e583b24d62e
Ivan Arellano <ivanarellano1418@gmail.com>
2025-05-28 12:04:17 -0700

    🔧 Tienda conectada a API con manejo de errores mejorado 1.1.16 bug fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
     applicationId = "com.catsoupstudios.solo_leveling"
     minSdk = 21
     targetSdk = 34
-    versionCode = 15
-    versionName = "1.1.15"
+    versionCode = 16
+    versionName = "1.1.16"
 }
 
     buildTypes {
